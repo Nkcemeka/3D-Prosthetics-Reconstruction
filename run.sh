ea3ae9b123bdd27772d783dd5f72e3f3244c0328
Chukwuemeka Nkama <nkcemeka@gmail.com>
2023-08-05 14:11:22 +0200

    Added functionality to detect missing files and warn about improper command line arguments.

--- run.sh
+++ run.sh
@@ -14,12 +14,28 @@ USAGE
 EOF
 }
 
-# print usage details to screen for user debugging
-usage
+if [ "$#" -eq 0 ] || [ "$#" -gt 2 ]; then
+  echo "Number of command line arguments should be one or two!"
+  echo "See USAGE details below!"
+  usage
+  exit 1
+fi
 
 # Get arguments and run the necessary commands
 img_path="$1" # path to image
 flip_cnt="${2:-0}" # flip contour for CAD viewing (0 is False)
+
+# Check if image exists
+if [ ! -f "$img_path" ]; then
+  echo 'Image file does not exist'
+  exit 1
+fi
+
+# Check if inputted number is correct
+if ! [ "$flip_cnt" -eq 0] || ! ["$flip_cnt" -eq 1]; then
+  echo "Wrong integer passed; Input 0 or 1"
+  exit 1
+fi
 
 # Run the Prosths file
 python3 prosths.py "$img_path" "$flip_cnt" 
